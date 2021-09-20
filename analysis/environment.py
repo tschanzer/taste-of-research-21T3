cab179bff3af961c86f36b49d041bc8158ea2a79
Thomas Schanzer <76029848+tschanzer@users.noreply.github.com>
2021-09-21 02:08:43 +1000

    Update script for Environment class with new motion function

--- analysis/environment.py
+++ analysis/environment.py
@@ -17,16 +17,77 @@ from scipy.optimize import root_scalar
 import sys
 
 
+class MotionResult():
+    """
+    Class for results of parcel motion calculations.
+
+    Attributes:
+        height: An array of heights, with each row corresponding to a
+            different initial condition.
+        velocity: An array of velocitites, with each row corresponding
+            to a different initial condition.
+        neutral_buoyancy_time: Times at which parcels reached their
+            neutral buoyancy levels.
+        hit_ground_time: Times at which parcels hit the ground.
+        min_height_time: Times at which parcels reached their minimum
+            heights (without hitting the ground)
+        neutral_buoyancy_height: The heights of the neutral buoyancy
+            levels.
+        neutral_buoyancy_velocity: The velocities at the neutral
+            buoyancy levels.
+        hit_ground_velocity: The velocities with which the parcels hit
+            the ground.
+        min_height: The minimum heights of the parcels (that did not hit
+            the ground).
+    """
+
+    def __init__(
+            self, height, velocity, neutral_buoyancy_time, hit_ground_time,
+            min_height_time, neutral_buoyancy_height,
+            neutral_buoyancy_velocity, hit_ground_velocity, min_height):
+        """
+        Instantiates a MotionResult.
+        """
+
+        self.height = height
+        self.velocity = velocity
+        self.neutral_buoyancy_time = neutral_buoyancy_time
+        self.hit_ground_time = hit_ground_time
+        self.min_height_time = min_height_time
+        self.neutral_buoyancy_height = neutral_buoyancy_height
+        self.neutral_buoyancy_velocity = neutral_buoyancy_velocity
+        self.hit_ground_velocity = hit_ground_velocity
+        self.min_height = min_height
+
+
 class Environment():
+    """
+    Class for parcel calculations on a real atmospheric sounding.
+    """
+
     def __init__(self, pressure, temperature, dewpoint):
+        """
+        Instantiates an Environment.
+
+        Allows all variables to be calculated as functions of height
+        rather than pressure by assuming hydrostatic balance.
+
+        Args:
+            pressure: Pressure array in the sounding.
+            temperature: Temperature array in the sounding.
+            dewpoint: Dewpoint array in the sounding.
+        """
+
         self.pressure_raw = pressure.to(units.mbar)
         self.temperature_raw = temperature.to(units.celsius)
         self.dewpoint_raw = dewpoint.to(units.celsius)
 
         self.temperature_interp = interp1d(
-            self.pressure_raw.m, self.temperature_raw.m, kind='linear')
+            self.pressure_raw.m, self.temperature_raw.m,
+            fill_value='extrapolate')
         self.dewpoint_interp = interp1d(
-            self.pressure_raw.m, self.dewpoint_raw.m, kind='linear')
+            self.pressure_raw.m, self.dewpoint_raw.m,
+            fill_value='extrapolate')
 
         def environment_dzdp(pressure, height):
             """
@@ -59,8 +120,10 @@ class Environment():
         sol = solve_ivp(
             environment_dzdp, (p_max, p_min), [0],
             t_eval=np.arange(p_max, p_min - 1e-3, -10e2))
-        self.pressure_interp_from_height = interp1d(np.squeeze(sol.y), sol.t)
-        self.height_interp_from_pressure = interp1d(sol.t, np.squeeze(sol.y))
+        self.pressure_interp_from_height = interp1d(
+            np.squeeze(sol.y), sol.t, fill_value='extrapolate')
+        self.height_interp_from_pressure = interp1d(
+            sol.t, np.squeeze(sol.y), fill_value='extrapolate')
 
 
     def temperature_from_pressure(self, pressure):
@@ -301,89 +364,113 @@ class Environment():
 
         return np.squeeze(sol)/1e3*units.km
 
-    def motion_from_dq(self, time, dq_range, initial_height):
+    def modified_motion(self, time, initial_height, dq):
         """
         Calculates parcel motion for different specific humidity changes.
 
         Args:
             time: Array of time points.
+            initial_height: Array of initial heights.
             dq_range: Array of initial changes in specific humidity due
                 to evaporation.
-            initial_height: Initial height of the parcels.
 
         Returns:
-            An array of parcel heights and an array of vertical
-                velocities, with each row corresponding to a different
-                initial specific humidity change and each column
-                corresponding to a different point in time.
+            A MotionResult object.
         """
 
-        def motion_ode(
-                time, state, initial_height, specific_humidity_change):
-            buoyancy = self.parcel_buoyancy(
-                state[0]*units.meter, initial_height, specific_humidity_change)
-            return [state[1], buoyancy.magnitude]
-
-        dq_range = np.atleast_1d(dq_range)
-        initial_state = [initial_height.to(units.meter).m, 0]
-        height = np.zeros((len(dq_range), len(time)))
-        velocity = np.zeros((len(dq_range), len(time)))
+        # independent variables
+        initial_height = np.atleast_1d(initial_height).to(units.meter).m
+        dq = np.atleast_1d(dq)
+        if len(initial_height) != len(dq):
+            raise ValueError(
+                'Initial height and specific humidity change arrays must '
+                'have the same length.')
+        initial_state = [[z0, 0] for z0 in initial_height]
         time = time.to(units.second).m
 
-        for i in range(len(dq_range)):
-            sol = solve_ivp(
-                motion_ode, [np.min(time), np.max(time)], initial_state,
-                t_eval=time, args=(initial_height, dq_range[i]))
-            height[i,:] = sol.y[0,:]
-            velocity[i,:] = sol.y[1,:]
-            sys.stdout.write(
-                '\rCalculating profile {0} of {1}.'
-                '   '.format(i+1, len(dq_range)))
-
-        return (np.squeeze(height)*units.meter,
-                np.squeeze(velocity)*units.meter/units.second)
-
-    def motion_from_height(self, time, initial_height_range, dq):
-        """
-        Calculates parcel motion for different initial heights.
-
-        Args:
-            time: Array of time points.
-            initial_height_range: Array of initial parcel heights.
-            dq: Initial change in specific humidity due to evaporation.
-
-        Returns:
-            An array of parcel heights and an array of vertical
-                velocities, with each row corresponding to a different
-                initial specific humidity change and each column
-                corresponding to a different point in time.
-        """
-
         def motion_ode(
-                time, state, initial_height, specific_humidity_change):
+                time, state, initial_height, dq):
+            """
+            Defines the equation of motion for a parcel.
+            """
+
             buoyancy = self.parcel_buoyancy(
-                state[0]*units.meter, initial_height,
-                specific_humidity_change)
+                state[0]*units.meter, initial_height*units.meter, dq)
             return [state[1], buoyancy.magnitude]
 
-        initial_height_range = np.atleast_1d(
-            initial_height_range).to(units.meter)
-        initial_state = np.array([[z0.m, 0] for z0 in initial_height_range])
+        # event function for solve_ivp, zero when parcel reaches min height
+        min_height = lambda time, state, *args: state[1]
+        min_height.direction = 1  # find zero that goes from negative to positive
+        min_height.terminal = True  # stop integration at minimum height
 
-        height = np.zeros((len(initial_height_range), len(time)))
-        velocity = np.zeros((len(initial_height_range), len(time)))
-        time = time.to(units.second).m
+        # event function for solve_ivp, zero when parcel hits ground
+        hit_ground = lambda time, state, *args: state[0]
+        hit_ground.terminal = True  # stop integration at ground
 
-        for i in range(len(initial_height_range)):
-            sol = solve_ivp(
-                motion_ode, [np.min(time), np.max(time)],
-                initial_state[i,:], t_eval=time,
-                args=(initial_height_range[i], dq))
-            height[i,:] = sol.y[0,:]
-            velocity[i,:] = sol.y[1,:]
+        # prepare empty arrays for data
+        height = np.zeros((len(dq), len(time)))
+        height[:] = np.nan
+        velocity = np.zeros((len(dq), len(time)))
+        velocity[:] = np.nan
+
+        neutral_buoyancy_time = np.zeros(len(dq))
+        hit_ground_time = np.zeros(len(dq))
+        min_height_time = np.zeros(len(dq))
+
+        neutral_buoyancy_height = np.zeros(len(dq))
+        neutral_buoyancy_velocity = np.zeros(len(dq))
+        hit_ground_velocity = np.zeros(len(dq))
+        min_height_height = np.zeros(len(dq))
+
+        for i in range(len(dq)):
             sys.stdout.write(
                 '\rCalculating profile {0} of {1}.'
-                '   '.format(i+1, initial_state.shape[0]))
+                '   '.format(i+1, len(dq)))
 
-        return (np.squeeze(height)*units.meter,
-                np.squeeze(velocity)*units.meter/units.second)
+            # event function for solve_ivp, zero when parcel is neutrally
+            # buoyant
+            neutral_buoyancy = lambda time, state, *args: motion_ode(
+                time, state, initial_height[i], dq[i])[1]
+
+            sol = solve_ivp(
+                motion_ode,
+                [np.min(time), np.max(time)],
+                initial_state[i],
+                t_eval=time,
+                args=(initial_height[i], dq[i]),
+                events=[neutral_buoyancy, hit_ground, min_height])
+
+            height[i,:len(sol.y[0,:])] = sol.y[0,:]
+            velocity[i,:len(sol.y[1,:])] = sol.y[1,:]
+
+            # record times of events
+            # sol.t_events[i].size == 0 means the event did not occur
+            neutral_buoyancy_time[i] = (  # record only the first instance
+                sol.t_events[0][0] if sol.t_events[0].size > 0 else np.nan)
+            hit_ground_time[i] = (
+                sol.t_events[1][0] if sol.t_events[1].size > 0 else np.nan)
+            min_height_time[i] = (
+                sol.t_events[2][0] if sol.t_events[2].size > 0 else np.nan)
+
+            # record states at event times
+            neutral_buoyancy_height[i] = (  # record only the first instance
+                sol.y_events[0][0,0] if sol.y_events[0].size > 0 else np.nan)
+            neutral_buoyancy_velocity[i] = (  # record only the first instance
+                sol.y_events[0][0,1] if sol.y_events[0].size > 0 else np.nan)
+            hit_ground_velocity[i] = (
+                sol.y_events[1][0,1] if sol.y_events[1].size > 0 else np.nan)
+            min_height_height[i] = (
+                sol.y_events[2][0,0] if sol.y_events[2].size > 0 else np.nan)
+
+        result = MotionResult(
+            np.squeeze(height)*units.meter,
+            np.squeeze(velocity)*units.meter/units.second,
+            np.squeeze(neutral_buoyancy_time)*units.second,
+            np.squeeze(hit_ground_time)*units.second,
+            np.squeeze(min_height_time)*units.second,
+            np.squeeze(neutral_buoyancy_height)*units.meter,
+            np.squeeze(neutral_buoyancy_velocity)*units.meter/units.second,
+            np.squeeze(hit_ground_velocity)*units.meter/units.second,
+            np.squeeze(min_height_height)*units.meter)
+
+        return result
